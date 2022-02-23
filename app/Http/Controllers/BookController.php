c60631fbc08d14d7f266fd52d74e0d01a7ef98d1
Romaslan <93395762+Romaslan@users.noreply.github.com>
2022-02-23 19:30:23 +0200

    new book with author add

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -63,7 +63,8 @@ class BookController extends Controller
      */
     public function create()
     {
-        //
+        $author = Author::all();
+        return view('book.create', ['authors'=>$author]);
     }
 
     /**
@@ -72,9 +73,30 @@ class BookController extends Controller
      * @param  \App\Http\Requests\StoreBookRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreBookRequest $request)
+    public function store(Request $request)
     {
-        //
+        $book = new Book;
+
+        $book->title=$request->book_title;
+        $book->description=$request->book_description;
+        if($request->book_newauthor){
+            $author = new Author;
+
+            $author->name=$request->author_name;
+            $author->surname=$request->author_surname;
+            $author->username=$request->author_username;
+            $author->description=$request->author_description;
+            $author->save();
+
+            $book->author_id = $author->id;
+
+        }else {
+            $book->author_id=$request->book_authorid;
+        }
+
+        $book->save();
+        return redirect()->route('book.index');
+
     }
 
     /**
